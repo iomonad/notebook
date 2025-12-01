0c36e25acef5d5c58c683f842c515027c3a87e6a
iomonad <iomonad@riseup.net>
2025-12-01 23:58:48 +0100

    fix[build] recursively build notebook

--- dev/build.clj
+++ dev/build.clj
@@ -2,6 +2,7 @@
   (:require [nextjournal.clerk :as clerk]))
 
 (defn -main [& _args]
-  (clerk/build! {:paths ["notebooks/*"]
+  (clerk/build! {:paths ["notebooks/**"]
+                 :package :directory
                  ;;;:index "notebooks/index.clj"
                  }))
