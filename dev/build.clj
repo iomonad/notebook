dbd2129a1040114c7cd5daa2a9f23f5d820a95db
iomonad <iomonad@riseup.net>
2025-12-13 13:18:40 +0100

    fix[ci] updated structure

--- dev/build.clj
+++ dev/build.clj
@@ -3,4 +3,6 @@
 
 (defn -main [& _args]
   (clerk/build! {:paths ["notebooks/**"]
+                 ;;:compile-css true
+                 :index "notebooks/landing.clj"
                  :package :directory}))
---
+++ notebooks/landing.clj
@@ -0,0 +1,25 @@
+^{:nextjournal.clerk/visibility {:code :hide}}
+(ns landing
+  (:require [nextjournal.clerk :as clerk]))
+
+;;; # Notebooks
+
+;;; Here are my notes, used to expose my mess playing around data on the internet.
+
+;;; ## Data Analysis
+
+;;; ### Technical Infrastructures)
+
+;;; ## Experimentations
+
+;;; ## Challenges
+
+;;; Here is a list of some code challenge, solved using **Clojure** notebook
+
+;;; ### Advent-of-Code
+
+;;; Advent of Code is an Advent calendar of small programming puzzles for a variety of skill levels that can be solved in any programming language you like.
+
+;;; ### Project Euler
+
+;;; *Project Euler* is a series of challenging mathematical/computer programming problems that will require **more** than just mathematical insights to solve.
