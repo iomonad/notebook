e3052353ef837a24f69cde146911051ea10014e2
iomonad <iomonad@riseup.net>
2025-12-13 14:48:06 +0100

    wip[grid] reduced gride size usage

--- src/clerk/components/tiles.clj
+++ src/clerk/components/tiles.clj
@@ -26,7 +26,7 @@
             (fn [{:keys [path preview-image title description]}]
               [:a.rounded-lg.shadow-lg.border.border-gray-300.relative.flex.flex-col.hover:border-indigo-600.group.mb-8.md:mb-0
                {:href (clerk/doc-url path) :title path :style {:height 300}}
-               [:div.flex-auto.overflow-hidden.rounded-t-md.flex.item-center.px-3.py-4
+               [:div.flex-auto.overflow-hidden.rounded-t-md.flex.items-center.px-3.py-4
                 [:img {:src preview-image :width "100%" :style {:object-fit "contain"}}]]
                [:div.sans-serif.border-t.border-gray-300.px-4.py-2.group-hover:border-indigo-600
                 [:div.font-bold.block.group-hover:text-indigo-600 title]
